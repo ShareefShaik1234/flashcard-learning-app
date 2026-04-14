0c586cf071229dccd0ac17407f3e3fbeccfcac9c
SHAIK BABA SHAREEF <shareefshaik63411@gmail.com>
2026-04-14 12:48:49 +0530

    Update flashcards.js

--- api/flashcards.js
+++ api/flashcards.js
@@ -1,1 +1,21 @@
 
+let cards = [];
+
+export default function handler(req, res) {
+
+  if (req.method === "GET") {
+    res.status(200).json(cards);
+  }
+
+  else if (req.method === "POST") {
+    const { question, answer } = req.body;
+    const newCard = { id: Date.now(), question, answer };
+    cards.push(newCard);
+    res.status(200).json(newCard);
+  }
+
+  else if (req.method === "DELETE") {
+    cards = [];
+    res.status(200).json({ message: "All deleted" });
+  }
+}
