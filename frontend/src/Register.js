f04e1619db60563546f87db2a96af3452f0da0b4
SHAIK BABA SHAREEF <shareefshaik63411@gmail.com>
2026-04-14 13:08:51 +0530

    Update Register.js

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,26 +1,44 @@
 import { useState } from "react";
-import axios from "axios";
 
 function Register({ setPage }) {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleRegister = () => {
-  if (!email || !password) {
-    alert("Enter details");
-    return;
-  }
 
-  alert("Registered successfully");
-  setPage("login");
-};
+    if (!email || !password) {
+      alert("Enter email and password");
+      return;
+    }
+
+    // store user locally
+    localStorage.setItem("username", email);
+
+    alert("Registered successfully");
+    setPage("login");
+  };
 
   return (
     <div>
       <h2>Register</h2>
-      <input placeholder="Email" onChange={e => setEmail(e.target.value)} />
-      <input placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
-      <button onClick={handleRegister}>Register</button>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleRegister}>
+        Register
+      </button>
     </div>
   );
 }
